313cd66728cb1a8a22e04e1542ca6c60d3efe861
yaxon <yaxon@mail.ru>
2019-10-26 22:44:21 +0300

    add libray native-base

--- MobileReactNative/App.js
+++ MobileReactNative/App.js
@@ -1,109 +1,87 @@
-import React, {Component} from 'react';
-import {Alert, Button, FlatList, InputAccessoryView, ScrollView, StyleSheet, Text, TextInput, View} from 'react-native';
+// import React, {Component} from 'react';
+// import {Alert, Button, FlatList, InputAccessoryView, ScrollView, StyleSheet, Text, TextInput, View} from 'react-native';
+// import UserForm from "./UserForm";
+//
+//
+// export default class App extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {};
+//     }
+//
+//     render() {
+//         let users = [
+//             {key: 'Devin'},
+//             {key: 'Dan'},
+//             {key: 'Dominic'},
+//             {key: 'Jackson'},
+//             {key: 'James'},
+//             {key: 'Joel'},
+//             {key: 'John'},
+//             {key: 'Jillian'},
+//             {key: 'Jimmy'},
+//         ];
+//
+//         const inputAccessoryViewID1 = "uniqueID1";
+//         const inputAccessoryViewID2 = "uniqueID2";
+//
+//         return (
+//             <View>
+//                 <UserForm />
+//                 <FlatList
+//                     data={users}
+//                     renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+//                 />
+//             </View>
+//         );
+//     }
+// }
 
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        paddingTop: 22
-    },
-    item: {
-        padding: 10,
-        fontSize: 18,
-        height: 44,
-    },
-})
 
-export default class App extends Component {
+import React from 'react';
+import { AppLoading } from 'expo';
+import { Container, Header, Content, Form, Item, Input } from 'native-base';
+import * as Font from 'expo-font';
+import { Ionicons } from '@expo/vector-icons';
+
+export default class App extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {};
-
-        this.handleInputChange = this.handleInputChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+        this.state = {
+            isReady: false,
+        };
     }
 
-    handleInputChange(event) {
-        console.warn('event', event);
-        const target = event.target;
-        const value = target.type === 'checkbox' ? target.checked : target.value;
-        const name = target.name;
-        // console.warn('name', name);
-        // console.warn('value', value);
-        
-        this.setState({
-            [name]: value
-        },
-       //     () => console.warn('this.state', this.state)
-        );
-        
-        
-    }
-
-    handleSubmit(event) {
-        Alert.alert('Simple Button pressed', this.state);
-        
-       // console.warn(this.state);
-        event.preventDefault();
+    async componentDidMount() {
+        await Font.loadAsync({
+            Roboto: require('native-base/Fonts/Roboto.ttf'),
+            Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+            ...Ionicons.font,
+        });
+        this.setState({ isReady: true });
     }
 
     render() {
-        let users = [
-            {key: 'Devin'},
-            {key: 'Dan'},
-            {key: 'Dominic'},
-            {key: 'Jackson'},
-            {key: 'James'},
-            {key: 'Joel'},
-            {key: 'John'},
-            {key: 'Jillian'},
-            {key: 'Jimmy'},
-        ];
-
-        const inputAccessoryViewID1 = "uniqueID1";
-        const inputAccessoryViewID2 = "uniqueID2";
+        
+        if (!this.state.isReady) {
+            return <AppLoading />;
+        }
 
         return (
-            <View>
-                <ScrollView keyboardDismissMode="interactive">
-                   
-                            <Text> Add User name: </Text>
-                            <TextInput
-                                style={{
-                                    padding: 10,
-                                    paddingTop: 50,
-                                }}
-                                name="name"
-                                type="string"
-                                value={this.state.name}
-                                inputAccessoryViewID={inputAccessoryViewID1}
-                                onChange={this.handleInputChange}
-                            />
-                            
-                            <Text> Add User age: </Text>
-                            <TextInput
-                                style={{
-                                    padding: 10,
-                                    paddingTop: 50,
-                                }}
-                                name="age"
-                                type="number"
-                                value={this.state.age}
-                                inputAccessoryViewID={inputAccessoryViewID2}
-                                onChange={this.handleInputChange}
-                            />
-                        <Button
-                            title="Press me"
-                            onPress={this.handleSubmit}
-                        />
-
-                </ScrollView>
-
-                <FlatList
-                    data={users}
-                    renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
-                />
-            </View>
+            <Container>
+                <Header />
+                <Content>
+                    <Form>
+                        <Item>
+                            <Input placeholder="Username" />
+                        </Item>
+                        <Item last>
+                            <Input placeholder="Password" />
+                        </Item>
+                    </Form>
+                </Content>
+            </Container>
         );
     }
 }
---
+++ MobileReactNative/UserForm.js
@@ -0,0 +1,70 @@
+﻿import React, {Component} from 'react';
+import {Alert, Button, FlatList, InputAccessoryView, ScrollView, StyleSheet, Text, TextInput, View} from 'react-native';
+
+
+const useStyles = makeStyles(theme => ({
+    container: {
+        display: 'flex',
+        flexWrap: 'wrap',
+    },
+    textField: {
+        marginLeft: theme.spacing(1),
+        marginRight: theme.spacing(1),
+        width: 200,
+    },
+    dense: {
+        marginTop: 19,
+    },
+    menu: {
+        width: 200,
+    },
+}));
+
+export default function UserForm() {
+    const classes = useStyles();
+    const [values, setValues] = React.useState({
+        name: 'Cat in the Hat',
+        age: '',
+        multiline: 'Controlled',
+        currency: 'EUR',
+    });
+
+    const handleChange = name => event => {
+        setValues({...values, [name]: event.target.value});
+    };
+
+    const handleSubmit = () => {
+
+    };
+
+    return (
+        <View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
+            <form className={classes.container} noValidate autoComplete="off">
+                <Text> Add User name: </Text>
+
+                <TextField
+                    id="standard-name"
+                    label="Name"
+                    className={classes.textField}
+                    value={values.name}
+                    onChange={handleChange('name')}
+                    margin="normal"
+                />
+
+                <Text> Add User age: </Text>
+                <TextField
+                    id="standard-age"
+                    label="Age"
+                    className={classes.textField}
+                    value={values.age}
+                    onChange={handleChange('age')}
+                    margin="normal"
+                />
+                <Button
+                    title="Press me"
+                    onPress={handleSubmit}
+                />
+            </form>
+        </View>
+    )
+}
