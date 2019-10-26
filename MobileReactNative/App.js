3030fcd5fb46d34e95454f9a6a686587ee69c8db
yaxon <yaxon@mail.ru>
2019-10-26 14:38:16 +0300

    add inputs

--- MobileReactNative/App.js
+++ MobileReactNative/App.js
@@ -1,19 +1,109 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import {Alert, Button, FlatList, InputAccessoryView, ScrollView, StyleSheet, Text, TextInput, View} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!)))</Text>
-    </View>
-  );
-}
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    container: {
+        flex: 1,
+        paddingTop: 22
+    },
+    item: {
+        padding: 10,
+        fontSize: 18,
+        height: 44,
+    },
+})
+
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleInputChange(event) {
+        console.warn('event', event);
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+        // console.warn('name', name);
+        // console.warn('value', value);
+        
+        this.setState({
+            [name]: value
+        },
+       //     () => console.warn('this.state', this.state)
+        );
+        
+        
+    }
+
+    handleSubmit(event) {
+        Alert.alert('Simple Button pressed', this.state);
+        
+       // console.warn(this.state);
+        event.preventDefault();
+    }
+
+    render() {
+        let users = [
+            {key: 'Devin'},
+            {key: 'Dan'},
+            {key: 'Dominic'},
+            {key: 'Jackson'},
+            {key: 'James'},
+            {key: 'Joel'},
+            {key: 'John'},
+            {key: 'Jillian'},
+            {key: 'Jimmy'},
+        ];
+
+        const inputAccessoryViewID1 = "uniqueID1";
+        const inputAccessoryViewID2 = "uniqueID2";
+
+        return (
+            <View>
+                <ScrollView keyboardDismissMode="interactive">
+                   
+                            <Text> Add User name: </Text>
+                            <TextInput
+                                style={{
+                                    padding: 10,
+                                    paddingTop: 50,
+                                }}
+                                name="name"
+                                type="string"
+                                value={this.state.name}
+                                inputAccessoryViewID={inputAccessoryViewID1}
+                                onChange={this.handleInputChange}
+                            />
+                            
+                            <Text> Add User age: </Text>
+                            <TextInput
+                                style={{
+                                    padding: 10,
+                                    paddingTop: 50,
+                                }}
+                                name="age"
+                                type="number"
+                                value={this.state.age}
+                                inputAccessoryViewID={inputAccessoryViewID2}
+                                onChange={this.handleInputChange}
+                            />
+                        <Button
+                            title="Press me"
+                            onPress={this.handleSubmit}
+                        />
+
+                </ScrollView>
+
+                <FlatList
+                    data={users}
+                    renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+                />
+            </View>
+        );
+    }
+}
