f06e521c893ac1d2904506c681fe6998ba3e4512
yaxon <yaxon@mail.ru>
2019-10-27 16:04:55 +0300

    remove coments

--- MobileReactNative/App.js
+++ MobileReactNative/App.js
@@ -1,49 +1,8 @@
-// import React, {Component} from 'react';
-// import {Alert, Button, FlatList, InputAccessoryView, ScrollView, StyleSheet, Text, TextInput, View} from 'react-native';
-// import UserForm from "./UserForm";
-//
-//
-// export default class App extends Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {};
-//     }
-//
-//     render() {
-//         let users = [
-//             {key: 'Devin'},
-//             {key: 'Dan'},
-//             {key: 'Dominic'},
-//             {key: 'Jackson'},
-//             {key: 'James'},
-//             {key: 'Joel'},
-//             {key: 'John'},
-//             {key: 'Jillian'},
-//             {key: 'Jimmy'},
-//         ];
-//
-//         const inputAccessoryViewID1 = "uniqueID1";
-//         const inputAccessoryViewID2 = "uniqueID2";
-//
-//         return (
-//             <View>
-//                 <UserForm />
-//                 <FlatList
-//                     data={users}
-//                     renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
-//                 />
-//             </View>
-//         );
-//     }
-// }
-
-
 import React from 'react';
 import {AppLoading} from 'expo';
 import {Container, Header, Content, Form, Item, Input, List, ListItem, Text} from 'native-base';
 import * as Font from 'expo-font';
 import {Ionicons} from '@expo/vector-icons';
-import {FlatList} from "react-native-web";
 
 export default class App extends React.Component {
     constructor(props) {
@@ -57,7 +16,7 @@ export default class App extends React.Component {
 
     setModalVisible(event) {
         console.warn('click', event);
-}
+    }
 
     async componentDidMount() {
         await Font.loadAsync({
@@ -101,11 +60,11 @@ export default class App extends React.Component {
                         </Item>
                     </Form>
 
-                    <List dataArray={users} renderRow={ item =>
+                    <List dataArray={users} renderRow={item =>
                         <ListItem button onPress={() => this.setModalVisible(true, item)}>
-                        <Text>{item.key}</Text>
+                            <Text>{item.key}</Text>
                         </ListItem>
-                        } />
+                    }/>
                 </Content>
             </Container>
         );
